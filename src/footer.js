f225a3eface9d86960201e35625c607278d93229
dev-fa <franco.a.dev@proton.me>
2022-10-17 15:06:19 +0800

    Create footer

---
+++ src/footer.js
@@ -0,0 +1,19 @@
+export default function footer() {
+    const footer = document.createElement("footer");
+    footer.classList.add("footer");
+
+    const githubLink = document.createElement("a");
+    githubLink.setAttribute("href", "https://github.com/dev-fa");
+    githubLink.setAttribute("target", "_blank");
+    githubLink.textContent = "dev-fa"
+    githubLink.classList.add("footer-text")
+
+    const footerText = document.createElement("span");
+    footerText.textContent = "Made by ";
+    footerText.classList.add("footer-text");
+
+    footer.appendChild(footerText);
+    footer.appendChild(githubLink);
+
+    return footer;
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import "./style.css";
 import Header from "./header.js";
+import Footer from "./footer.js";
 
-
-console.log("Test");
 
 const header = Header();
 document.body.appendChild(header);
+
+const content = document.createElement("main");
+content.setAttribute("id", "content");
+document.body.appendChild(content);
+
+const footer = Footer();
+document.body.appendChild(footer);
